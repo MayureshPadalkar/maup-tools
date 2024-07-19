29f59599e09cad8c1993d08349d8ba3541405ea0
MayureshPadalkar <160482033+MayureshPadalkar@users.noreply.github.com>
2024-07-19 16:17:31 +0100

    Add files via upload

---
+++ Home.py
@@ -0,0 +1,51 @@
+import streamlit as st
+
+st.set_page_config(
+	page_title="Streamlit Multipage App"
+)
+
+st.title("Modelling checks and Geospatial applications for Flood Modellers")
+st.logo("images/logo.png", icon_image="images/logo.png")
+# st.sidebar.success("Select a page above.")
+
+st.markdown(
+    """
+    This multi-page web app can be used for the following:  
+        - Mass balance checks from TUFLOW output  
+        - Courant numbers checks from HPC TUFLOW output  
+        - Histogram for plotting stage difference rasters  
+        - Rating curve calculator for a circular pipe  
+        - Manning's to Colebrook-White roughness converter
+
+    """
+)
+
+st.info("Click on the left sidebar menu to navigate to the different apps.")
+
+st.subheader("Developed by")
+st.image("images/developedby.gif")
+
+st.subheader("Acknowledgements")
+st.markdown(
+    """
+    To my family, colleagues, and alma mater.
+
+    """
+)
+
+st.sidebar.title("Contact")
+st.sidebar.info(
+    """
+    Mayuresh Padalkar for debugging, suggestions and improvements in the app on  
+    - mayuresh.padalkar@gmail.com  
+        
+    """
+)
+
+st.sidebar.title("Follow")
+st.sidebar.info(
+    """
+    Mayuresh Padalkar on  
+    - [LinkedIn](https://www.linkedin.com/in/mayuresh-padalkar-830b4817/)
+    """
+)
